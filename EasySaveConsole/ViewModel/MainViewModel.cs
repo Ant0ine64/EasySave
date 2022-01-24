c5efbb629e2a2cd79fd054805f1362f3a06709b8
Antoine Lonné <contact@ant0ine64.fr>
2022-01-24 22:20:46 +0100

    Adapt view to match requirements

--- EasySaveConsole/ViewModel/MainViewModel.cs
+++ EasySaveConsole/ViewModel/MainViewModel.cs
@@ -5,8 +5,12 @@ namespace EasySaveConsole.ViewModel;
 public class MainViewModel
 {
     private Save save = new Save();
-    
-    public void StartSavingJob(string source, string destination)
+
+    public void StartSavingJob(string name)
+    {
+        
+    }
+    public void CreateSavingJob(string name, string source, string destination)
     {
         //Save
         //Write state file 
