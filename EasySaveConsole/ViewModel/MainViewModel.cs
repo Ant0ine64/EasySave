c5b093128e16bceaf028f9f39b7419908c81d326
Antoine Lonné <contact@ant0ine64.fr>
2022-01-24 19:00:27 +0100

    Create base structure (also basic prompt: start of #4)

---
+++ EasySaveConsole/ViewModel/MainViewModel.cs
@@ -0,0 +1,16 @@
+using EasySaveConsole.Service;
+
+namespace EasySaveConsole.ViewModel;
+
+public class MainViewModel
+{
+    private Save save = new Save();
+    
+    public void StartSavingJob(string source, string destination)
+    {
+        //Save
+        //Write state file 
+        
+        // at the end : write log file
+    }
+}
