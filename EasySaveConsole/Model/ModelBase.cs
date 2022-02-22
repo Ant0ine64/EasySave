e5623ca77d3b33ad46f931e0c01f9baee9fd3d1c
Antoine <contact@ant0ine64.fr>
2022-02-22 09:28:37 +0100

    Add job deletion #39

--- EasySaveConsole/Model/ModelBase.cs
+++ EasySaveConsole/Model/ModelBase.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using JetBrains.Annotations;
 
 namespace EasySaveConsole.Model
 {
--- EasySaveUI/ViewModels/MainWindowViewModel.cs
+++ EasySaveUI/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using EasySaveUI.Views;
 using ReactiveUI;
 using System.Windows.Input;
@@ -11,16 +13,50 @@ namespace EasySaveUI.ViewModels
     public class MainWindowViewModel : ViewModelBase
     {
         public ICommand OnClickCreated { get; private set; }
+        public ICommand OnClickDelete { get; private set; }
+        public ICommand OnClickStart { get; private set; }
+        public ICommand OnClickSelectAll { get; private set; }
+        private bool selectedAll = false;
         public MainViewModel mvm = new MainViewModel();
         public ObservableCollection<Job> Jobs { get; set; }
 
         public MainWindowViewModel()
         {
             Jobs = new ObservableCollection<Job>(mvm.fetchSavingJob());
+            
             OnClickCreated = ReactiveCommand.Create(() =>
             {
                 CreatePage createPage = new CreatePage();
                 createPage.Show();
+            });
+            
+            OnClickDelete = ReactiveCommand.Create(() =>
+            {
+                var jobs = Jobs.ToList();
+                foreach (var job in jobs.Where(job => job.IsChecked))
+                {
+                    // remove visually
+                    Jobs.Remove(job);
+                    // remove in jobfile
+                    Job.Delete(job);
+                }
+            });
+            
+            OnClickStart = ReactiveCommand.Create(() =>
+            {
+                foreach (var job in Jobs)
+                {
+                    job.IsChecked = selectedAll;
+                }
+            });
+            
+            OnClickSelectAll = ReactiveCommand.Create(() =>
+            {
+                selectedAll = !selectedAll;
+                foreach (var job in Jobs)
+                {
+                    job.IsChecked = selectedAll;
+                }
             });
         }
     }
