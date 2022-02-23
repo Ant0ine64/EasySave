6a591d256ec35d5040820f53160d1e13d9df457e
Josh <jdelriu@entelekia.fr>
2022-02-23 10:47:31 +0100

    fonction commenté

--- EasySaveUI/ViewModels/ViewModelBase.cs
+++ EasySaveUI/ViewModels/ViewModelBase.cs
@@ -6,6 +6,11 @@ namespace EasySaveUI.ViewModels
 {
     public class ViewModelBase : ReactiveObject
     {
+        /// <summary>
+        /// update the content and datacontext of MainWindow
+        /// </summary>
+        /// <param name="content">exemple:logsWindow.Content</param>
+        /// <param name="dataContext">exmplelogsWindow.DataContext</param>
         public void updateContent(object content, object dataContext)
         {
             if (Avalonia.Application.Current.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
